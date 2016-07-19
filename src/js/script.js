9128fe71f6c9f21b71c2de3971c837ac612d8b4b
Rustam Yusupov <rustam.yusupov@gmail.com>
2016-07-19 14:51:43 +0500

    js меню

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+  var menuTrigger = document.querySelector('.menu__trigger');
+
+  menuTrigger.addEventListener('click', showMenu);
+
+  function showMenu() {
+    var menu = document.querySelector('.menu');
+
+    menu.classList.toggle('menu--show');
+  }
+});
